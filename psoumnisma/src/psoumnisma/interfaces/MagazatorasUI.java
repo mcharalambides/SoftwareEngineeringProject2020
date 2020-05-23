bfbbb5a03fc9067c72334342467ff5f5cc9fb0b3
mcharalambides <marios-1996@windowslive.com>
2020-05-23 18:44:53 +0300

    Magazatoras Interface table names included

--- psoumnisma/src/psoumnisma/interfaces/MagazatorasUI.java
+++ psoumnisma/src/psoumnisma/interfaces/MagazatorasUI.java
@@ -94,9 +94,17 @@ public class MagazatorasUI extends javax.swing.JFrame {
                 {null, null, null, null}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "Full Name", "Address", "Telephone", "List"
             }
-        ));
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.Object.class
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+        });
         jScrollPane1.setViewportView(jTable1);
 
         jLabel7.setBackground(new java.awt.Color(255, 153, 0));
