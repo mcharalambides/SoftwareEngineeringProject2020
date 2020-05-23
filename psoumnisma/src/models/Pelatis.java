c17c60627a8eb9306d1f2f85a98c11733816322e
mcharalambides <marios-1996@windowslive.com>
2020-05-23 19:04:02 +0300

    Added properties to pelatis model

---
+++ psoumnisma/src/models/Pelatis.java
@@ -0,0 +1,11 @@
+package models;
+
+public class Pelatis {
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+    private int telephoneNumber;
+    private String address;
+    private int coordinates[];
+}
