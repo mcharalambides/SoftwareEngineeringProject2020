7909dd389e561ee35f447a1628da786000d87604
dexkali10 <luapk1996@hotmail.com>
2020-05-26 17:26:08 +0300

    Rate interface

---
+++ psoumnisma/src/models/PremiumPelatis.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import java.util.List;
+
+/**
+ *
+ * @author Pavlos
+ */
+public class PremiumPelatis extends Pelatis {
+    private int points;
+   private List<Prosfora> offers;
+  // private List<Proion> favouriteItemsList;         
+            
+//public void setOffers(List<Prosfora> offers){
+   //this.offers = new List<Prosfora>(offers);
+//}
+
+    public int getPoints() {
+        return points;
+    }
+
+
+   
+}
