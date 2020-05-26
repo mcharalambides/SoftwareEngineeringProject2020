41a3097570934c034679ecdf2be12a95068bf4b5
dexkali10 <luapk1996@hotmail.com>
2020-05-26 17:44:34 +0300

    palia lista (incomplete)

---
+++ psoumnisma/src/models/PaliesListes.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author Pavlos
+ */
+public class PaliesListes {
+    private String customerName;
+    private String storeName;
+    private double distance;
+    private Proion itemList;
+    private int ID;
+    
+    //public PaliesListes getPaliaLista(ID){
+    
+//}
+}
