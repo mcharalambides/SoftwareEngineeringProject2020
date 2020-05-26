a4987657c47856772dcd815213c16af435e6d167
rafaelneof <rafaelneof@gmail.com>
2020-05-26 17:48:02 +0300

    Added Java class PinakasApotelesmatwn

---
+++ psoumnisma/src/models/PinakasApotelesmatwn.java
@@ -0,0 +1,19 @@
+
+package models;
+import java.io.File;
+
+public class PinakasApotelesmatwn {
+    private String storeName;
+    private File storeImage;
+    private Integer cost;
+    private Double distance;
+    private Integer storeRating;
+    
+   // public Boolean checkMagazatores(lista, magazatoras) {    
+    
+    //this.lista=lista
+    //this.magazatoras=magazatoras
+    
+    //}
+    
+}
